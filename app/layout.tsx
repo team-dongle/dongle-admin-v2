df5737b702d69c238047dbc4c48196e4d3b15699
조용원 <auau3@kakao.com>
2024-12-04 17:49:02 +0900

    fix: metadata/viewport 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,24 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/styles/global.css";
 
 export const metadata: Metadata = {
   title: "동글 관리자",
   description: "우리의 동아리, 우리의 동글",
+  metadataBase: new URL("https://admin.dongle.world"),
+  openGraph: {
+    images: [{ url: "./thumbnail.png", alt: "동글 썸네일" }],
+    locale: "ko",
+    url: "https://admin.dongle.world",
+    description: "우리의 동아리, 우리의 동글",
+    type: "website",
+    siteName: "동글 관리자",
+  },
+};
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  initialScale: 1,
+  width: "device-width",
 };
 
 export default function RootLayout({
