1462a9453fa4f4d6c3b5153cfc40d5be12e3d419
조용원 <auau3@kakao.com>
2024-12-19 17:38:38 +0900

    feature: 활동보고서 관리 유의사항 컴포넌트 추가 - 활동보고서 작성 시 유의사항을 담은 컴포넌트 최상단에 추가

--- app/(service)/@club/reports/page.tsx
+++ app/(service)/@club/reports/page.tsx
@@ -14,6 +14,20 @@ export default async function Page() {
   return (
     <section className="relative h-full w-full">
       <h1 className="mb-12 text-3xl font-normal">활동보고서 관리</h1>
+      <div className="mb-4 h-auto w-full rounded-md bg-white p-4 shadow">
+        <h2 className="mb-2 text-lg font-bold text-gray-700">Information</h2>
+        <ol className="flex list-decimal flex-col gap-1.5 pl-4">
+          <li className="text-sm text-gray-700">
+            활동보고서 제목은 5자 이상, 20자 미만으로 작성 가능합니다.
+          </li>
+          <li className="text-sm text-gray-700">
+            활동보고서 본문은 100자 이상, 1000자 미만으로 작성 가능합니다.
+          </li>
+          <li className="text-sm text-gray-700">
+            활동 사진은 최소 1장, 최대 3장 업로드 가능합니다.
+          </li>
+        </ol>
+      </div>
       <ReportList reports={reportList.result.rows} />
     </section>
   );
