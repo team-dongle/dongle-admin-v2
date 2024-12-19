90ad490dc13e85c79f1258a803849638c08dab36
조용원 <auau3@kakao.com>
2024-12-19 17:39:08 +0900

    fix: 활동보고서 제목, 본문 validation 변경 - 활동보고서 제목: min 5, max 20 - 활동보고서 본문: min 100, max: 1000

--- actions/validators/report.ts
+++ actions/validators/report.ts
@@ -4,10 +4,14 @@ import { urlRegexp } from "@/utils/regex";
 
 export const isCreateReportFormValid = (payload: ReportPayload) => {
   const shape = z.object({
-    title: z.string().min(1, { message: "활동보고서 제목을 입력해 주세요." }),
+    title: z
+      .string()
+      .min(5, { message: "활동보고서 제목은 최소 5자 이상이여야 합니다." })
+      .max(20, { message: "활동보고서 제목은 최대 20자 이하여야 합니다." }),
     content: z
       .string()
-      .min(100, { message: "활동보고서 본문은 최소 100자 이상이여야 합니다." }),
+      .min(100, { message: "활동보고서 본문은 최소 100자 이상이여야 합니다." })
+      .max(1000, { message: "활동보고서 본문은 최대 1000자 이하여야 합니다." }),
     images: z
       .array(
         z.object({
